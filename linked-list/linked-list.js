615aed52b64c6f6802aef0aa9b7ab8e3337b0240
Martin Balke <Mrtnbalke@gmail.com>
2019-10-17 19:59:57 -0700

    comments

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//Creating a class for the individual nodes
 class Node {
   constructor(data) {
     this.next = null;
@@ -7,36 +7,59 @@ class Node {
   }
 }
 
-class LinkedList{
-  constructor(){
+//instantiating the linkedlist class
+class LinkedList {
+  constructor() {
     this.head = null;
   }
-  
-  insert(value){
+  //insert a node at the beigining of the list
+  insert(value) {
     let newNode = new Node(value);
-    if(this.head)newNode.next = this.head;
+    //Check to see if there is already a head 
+    //If there is change the new node's next to be head before making it the new head
+    if (this.head) newNode.next = this.head;
     this.head = newNode;
     return this;
   }
 
-  includes(val){
+  includes(val) {
+    //Set up a variable to keep track of the current node we're on
     let current = this.head;
-    while(current.next !== null){
-      if(current.data === val) return true;
+    //While there is a next node
+    while (current.next !== null) {
+      //Check the current node to see if it is a match
+      //If not cycle to the next node
+      if (current.data === val) return true;
       current = current.next;
     }
+    //Return for the last node in the list
     return current.data === val;
   }
-
-  toString(){
+  //Print the linked list as a simple string
+  toString() {
     let string = '';
+    //Set a variable to keep track of the current node
     let current = this.head;
+    //While there is a next node
     while (current.next !== null) {
+      //Add this nodes data to the string
       string += current.data.toString();
       current = current.next;
     }
+    //Add the last nodes data to the string
     return string += current.data.toString();
   }
+  //Add a node to the end of the list
+  append(val) {
+    let newNode = new Node(val);
+    //If there is no head node this will make a head
+    if (!this.head) return this.head = newNode;
+    let current = this.head;
+    //Iterate over the linked list until the end
+    while (current.next !== null) {
+      current = current.next;
+    }
+    //Set the next of the last node as this new node
+    return current.next = newNode;
+  }
 }
-
-module.exports = LinkedList;
