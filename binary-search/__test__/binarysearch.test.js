876a61f7e1525c15045d7b08fcec2d73bae86a54
Martin Balke <Mrtnbalke@gmail.com>
2019-10-21 17:05:39 -0700

    everything working and up to date

--- binary-search/__test__/binarysearch.test.js
+++ binary-search/__test__/binarysearch.test.js
@@ -1,22 +1,20 @@
 'use strict';
 const binarySearch = require('../index');
 
+let arr = [1, 2, 3, 4, 5];
+
 describe('Binary search tests', () => {
-  let arr = [1, 2, 3, 4, 5];
-  let key;
-  let left = 0;
-  let right = arr.length - 1;
-  let mid = Math.floor((left + right) / 2);
+
   
   it('Returns -1 if key is not found', () => {
-    key = 6;
-    expect(binarySearch(mid,left,right)).toBe(-1);
+    let key = 6;
+    expect(binarySearch(arr, key)).toBe(-1);
   });
   
   it('Returns index if the key matches', () => {
-    key = 3;
-    expect(binarySearch(mid,left,right)).toBe(2);
+    let key = 3;
+    expect(binarySearch(arr, key)).toBe(2);
     key = 1;
-    expect(binarySearch(mid, left, right)).toBe(1);
+    expect(binarySearch(arr, key)).toBe(0);
   });
 });
--- binary-search/index.js
+++ binary-search/index.js
@@ -1,26 +1,43 @@
-let arr;
-let key;
-let left = 0;
-let right = arr.length - 1;
-let mid = Math.floor((left + right) / 2);
+//Declaring inputs array to search and key to search for
+let arr = [1,2,3,4,5,6,7,8];
+let key = 6;
 
 
-const binarySearch = (mid, left, right) => {
-  if(arr[mid] === key) return mid;
-  if(left > right) return -1;
-  if(arr[mid] > key){
-    right = mid -1;
-    mid = Math.floor((left + right) / 2);
-    return binarySearch(mid, left, right);
-  }
-  if(arr[mid] < key){
-    
-    left = mid + 1;
-    mid = Math.floor((left + right) / 2);
-    return binarySearch(mid, left, right);
-  }
+//Realized I need to put this function in a wrapper to be able to test it succesfully
+const binarySearchWrapper =  (arr,key) => {
+
+  console.log(arr,key);
+  //Declaring the left side of the array and the right side, then using those to determine the middle
+  let left = 0;
+  let right = arr.length - 1;
+  let mid = Math.floor((left + right) / 2);
+
+  //Function binary search is recursive
+  const binarySearch = (mid, left, right) => {
+    //If the middle of the array is equal to the key return it
+    if (arr[mid] === key) return mid;
+    //If left is ever bigger than right return -1 to signify not found
+    if (left > right) return -1;
+    //If the middle is greater than the key search left side
+    if (arr[mid] > key) {
+      right = mid - 1;
+      mid = Math.floor((left + right) / 2);
+      return binarySearch(mid, left, right);
+    }
+
+    //If the middle is less than the key search right side
+    if (arr[mid] < key) {
+      left = mid + 1;
+      mid = Math.floor((left + right) / 2);
+      return binarySearch(mid, left, right);
+    }
+  };
+
+  return binarySearch(mid, left, right);
 };
 
+console.log(binarySearchWrapper(arr,key));
 
 
-module.exports = binarySearch();
+
+module.exports = binarySearchWrapper;
