f28808b5bca1bda9ebff7699ff3f66de21051857
Martin Balke <Mrtnbalke@gmail.com>
2019-11-12 09:58:08 -0800

    commenting

--- tree/__test__/tree.test.js
+++ tree/__test__/tree.test.js
@@ -4,7 +4,12 @@ const BinarySearchTree = require('../tree');
 let binarySearchTree = new BinarySearchTree();
 
 describe('binary tree testing', () => {
-  it('', () => {
+  it('Can successfully instantiate an empty tree', () => {
     console.log(binarySearchTree);
   });
+  it('Can successfully instantiate a tree with a single root node', () => {});
+  it('Can successfully return a collection from a pre-order traversal', () => {});
+  it('Can successfully return a collection from an in-order traversal', () => {});
+  it('Can successfully return a collection from a post-order traversal', () => {});
+  it('Can successfully add a left child and right child to a single root node on a Binary Search Tree', () => {});
 });
