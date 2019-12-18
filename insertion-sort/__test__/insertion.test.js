502f5acc04e10269f6d4e263e481d1dc144c9b3e
Martin Balke <Mrtnbalke@gmail.com>
2019-12-17 18:20:26 -0800

    testing

--- insertion-sort/__test__/insertion.test.js
+++ insertion-sort/__test__/insertion.test.js
@@ -3,22 +3,34 @@ const insertionSort = require('../insertion');
 describe( 'insertion sort tests', () => {
   it('Sort array length of k', () => {
     let array1 = insertionSort([ 3 ]);
-    expect(array1).toBe([3]);
+    expect(array1).toEqual([3]);
 
     let array2 = insertionSort([3,2]);
-    expect(array2).toBe([2,3]);
+    expect(array2).toEqual([2,3]);
    
     let array3 = insertionSort([0,9,8,7,6,5,4,3,2,1]);
-    expect(array3).toBe([0,1,2,3,4,5,6,7,8,9]);
+    expect(array3).toEqual([0,1,2,3,4,5,6,7,8,9]);
 
   });
-  it('', () => {
-
+  it('Error when given not an array', () => {
+    try {
+      insertionSort({});
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
   });
-  it('', () => {
-
+  it('Error when empty array', () => {
+    try {
+      insertionSort([]);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
   });
-  it('', () => {
-
+  it('Error when one of the elements is not a number', () => {
+    try {
+      insertionSort([1,'a']);
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
   });
 });
